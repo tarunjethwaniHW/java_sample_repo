5d7e24ebcca36ceee4d92edd77c97f9915c9a1ef
tarunjethwaniHW <TarunJ@hexaware.com>
2026-07-02 08:44:41 +0530

    Add files via upload

---
+++ account-service/src/main/java/com/rapidx/accoutservice/messaging/RBqMessageProducer.java
@@ -0,0 +1,33 @@
+package com.rapidx.accoutservice.messaging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AmqMessageProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(AmqMessageProducer.class);
+
+    private final JmsTemplate jmsTemplate;
+    
+    @Value("${spring.rabbitmq.queue-name}")
+    private String queueName;
+
+    public AmqMessageProducer(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    public void publishEvent(String eventType, Long accountId, String accountName) {
+        String payload = String.format("{\"eventType\": \"%s\", \"accountId\": %d, \"accountName\": \"%s\"}", 
+                eventType, accountId, accountName);
+        log.info("Publishing JMS message to queue {}: {}", queueName, payload);
+        try {
+            jmsTemplate.convertAndSend("SF-BA0352-EBMQueue.local", payload);
+        } catch (Exception e) {
+            log.error("Failed to publish JMS message to queue {}", queueName, e);
+        }
+    }
+}
